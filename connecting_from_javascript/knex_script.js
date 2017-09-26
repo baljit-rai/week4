4f50b40729f03e75ac080d45ade8d6b9c1bd5567
Baljit Rai <bsrai91@gmail.com>
2017-09-26 22:15:50 +0000

    create knex_script

---
+++ connecting_from_javascript/knex_script.js
@@ -0,0 +1,38 @@
+const settings = require("./settings"); // settings.json
+
+// const name = process.argv.slice(2);
+const name = process.argv[2];
+
+var knex = require('knex')({
+  client: 'pg',
+  connection: {
+    user: settings.user,
+    password: settings.password,
+    database: settings.database,
+    host: settings.hostname,
+    port: settings.port,
+    ssl: settings.ssl
+  }
+});
+
+
+
+findPerson(name);
+
+function findPerson(lastname) {
+  console.log('Searching......');
+  knex.select('*').from('famous_people').where('last_name', lastname).asCallback(function(err, result) {
+    returnPerson(result);
+
+  });
+
+}
+
+
+function returnPerson(result) {
+  result.forEach(function(element) {
+    console.log('Found ' + result.length + " person(s)" + ': ' + element.first_name + ' ' +
+      element.last_name + ', ' + 'born' + " '" + element.birthdate.toISOString().substr(0, 10) + "'");
+
+  });
+}
